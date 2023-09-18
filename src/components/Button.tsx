21fb3950dfd12b3186ccc96c868af31948292222
Wicarpessoa <wicarpp@gmail.com>
2023-09-18 17:11:56 -0300

    first commit, styled components and eslint configured

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, ButtonVariant } from './button.styles'
+
+interface ButtonProps {
+  variant?: ButtonVariant
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
